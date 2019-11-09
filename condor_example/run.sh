b20a76858468898e04c542c4dc9b31f64a576848
root <root@cmsdas.phy.pku.edu.cn>
2019-11-09 12:39:04 -0500

    inital commit

---
+++ condor_example/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Where am I:       `pwd`"
+echo "What am I doing:  play with condor@PKU CMSDAS"
+echo "What's the time:"
+echo "  ... thinking really hard for 4 seconds..."
+sleep 4
+echo "                  `date +"%T"`"
+echo "Thanks condor, bye"
